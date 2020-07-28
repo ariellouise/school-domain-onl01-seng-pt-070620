da7e6bbeb95afe1e04ea8ca6dc392e0c92a85e03
ariel murphy <arielladders@gmail.com>
2020-07-28 11:21:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School(name)
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  Roster = []
+  
+  def name
+    @name 
+  end 
+  
+end 
+
+school= 
+School.new("Bayside High School")
+school.roster
